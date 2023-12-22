1fdc31bbf468ce04a84015a984868c4bc85f74d7
khaledgabr77 <khaledgabr77@gmail.com>
2023-12-22 19:25:05 +0300

    sync different msgs type

--- launch/random_trajectories.launch.py
+++ launch/random_trajectories.launch.py
@@ -88,6 +88,8 @@ def generate_launch_description():
                    '/d435/image@sensor_msgs/msg/Image[ignition.msgs.Image',
                    '/d435/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
                    '/d435/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                   '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+
                    '--ros-args', '-r', '/d435/depth_image:='+ns+'/depth_image',
                    '-r', '/d435/image:='+ns+'/image',
                    '-r', '/d435/points:='+ns+'/points',
--- uav_dataset_generation_ros/execute_random_trajectories_node.py
+++ uav_dataset_generation_ros/execute_random_trajectories_node.py
@@ -19,11 +19,14 @@ import cv2
 import os
 from cv_bridge import CvBridge
 import csv
+import message_filters
+from message_filters import ApproximateTimeSynchronizer
 
 
 
             
 class OffboardControl(Node):
+    QUEUE_SIZE = 50
 
     def __init__(self):
         super().__init__('random_trajectories_node')
@@ -117,7 +120,12 @@ class OffboardControl(Node):
             history=HistoryPolicy.KEEP_LAST,
             depth=1
         )
-
+        sensor_qos_profile = QoSProfile(
+            depth=50,  # A larger queue size to handle high-frequency sensor data
+            reliability=ReliabilityPolicy.BEST_EFFORT,  # Suitable for high-throughput data
+            durability=DurabilityPolicy.VOLATILE,  # Only interested in the most recent values
+            history=HistoryPolicy.KEEP_LAST  # Keep only a number of latest samples defined by the depth
+        )
         self.odom_ = Odometry() # latest odom
         self.rgb_image_ = Image() # latest image
         self.depth_image_ = Image() # latest image
@@ -129,32 +137,53 @@ class OffboardControl(Node):
             self.vehicleStatusCallback,
             qos_profile_transient)
         
-        self.odom_sub_ = self.create_subscription(
+        # self.odom_sub_ = self.create_subscription(
+        #     Odometry,
+        #     'mavros/local_position/odom',
+        #     self.odomCallback,
+        #     qos_profile_sensor_data)
+        
+        # self.rgb_image_ = self.create_subscription(
+        #     Image,
+        #     '/target/image',
+        #     self.imageCallback,
+        #     qos_profile_sensor_data)
+        
+        # self.depth_image_ = self.create_subscription(
+        #     Image,
+        #     '/target/depth_image',
+        #     self.depthCallback,
+        #     qos_profile_sensor_data)
+        
+        # Setpoint frequency
+        timer_period = 0.02  # seconds
+        self.cmd_timer_ = self.create_timer(timer_period, self.cmdloopCallback)
+        self.odom_sub_ = message_filters.Subscriber(
+            self,
             Odometry,
-            'mavros/local_position/odom',
-            self.odomCallback,
-            qos_profile_sensor_data)
-        
-        self.rgb_image_ = self.create_subscription(
+            'mavros/local_position/odom',qos_profile=sensor_qos_profile)
+        self.rgb_sub_ = message_filters.Subscriber(
+            self,
             Image,
-            '/target/image',
-            self.imageCallback,
-            qos_profile_sensor_data)
-        
-        self.depth_image_ = self.create_subscription(
+            'image',qos_profile=sensor_qos_profile)
+        self.depth_sub_  = message_filters.Subscriber(
+            self,
             Image,
-            '/target/depth_image',
-            self.depthCallback,
-            qos_profile_sensor_data)
+            'depth_image',qos_profile=sensor_qos_profile)
         
+        self.time_synchronizer = ApproximateTimeSynchronizer(
+            [self.odom_sub_, self.rgb_sub_, self.depth_sub_],
+            self.QUEUE_SIZE,
+            slop=0.02  # Adjust the slop value according to the acceptable time difference
+        )
+
+        self.time_synchronizer.registerCallback(self.dataCallback)
+
         self.vehicle_path_pub_ = self.create_publisher(Path, 'offboard_visualizer/vehicle_path', 10)
         self.setpoint_path_pub_ = self.create_publisher(Path, 'offboard_visualizer/setpoint_path', 10)
 
         self.setopint_pub_ = self.create_publisher(PositionTarget, 'mavros/setpoint_raw/local', qos_profile_sensor_data)
 
-        # Setpoint frequency
-        timer_period = 0.02  # seconds
-        self.cmd_timer_ = self.create_timer(timer_period, self.cmdloopCallback)
 
         self.offboard_setpoint_counter_ = 0
 
@@ -177,18 +206,18 @@ class OffboardControl(Node):
     def vehicleStatusCallback(self, msg: State):
         self.is_armed_ = msg.armed
 
-    def odomCallback(self, msg: Odometry):
-        self.odom_ = msg
+    # def odomCallback(self, msg: Odometry):
+    #     self.odom_ = msg
 
-    def imageCallback(self, msg: Image):
-        self.rgb_image_ = msg
-        cv_image = CvBridge().imgmsg_to_cv2(msg, "bgr8")  # Convert ROS Image message to OpenCV format
-        self.save_image(cv_image, self.rgb_img_directory_, "rgb")
+    # def imageCallback(self, msg: Image):
+    #     self.rgb_image_ = msg
+    #     cv_image = CvBridge().imgmsg_to_cv2(msg, "bgr8")  # Convert ROS Image message to OpenCV format
+    #     self.save_image(cv_image, self.rgb_img_directory_, "rgb")
     
-    def depthCallback(self, msg: Image):
-        self.depth_image_ = msg
-        cv_image = CvBridge().imgmsg_to_cv2(msg, "passthrough")  # Convert ROS Image message to OpenCV format
-        self.save_image(cv_image, self.depth_img_directory_, "depth")
+    # def depthCallback(self, msg: Image):
+    #     self.depth_image_ = msg
+    #     cv_image = CvBridge().imgmsg_to_cv2(msg, "passthrough")  # Convert ROS Image message to OpenCV format
+    #     self.save_image(cv_image, self.depth_img_directory_, "depth")
    
     def save_image(self, cv_image, directory, image_type):
         if image_type == "rgb":
@@ -197,6 +226,17 @@ class OffboardControl(Node):
             image_name = f"{self.file_name_}_{self.traj_counter_}_{self.offboard_setpoint_counter_}_depth.png"
         cv2.imwrite(os.path.join(directory, image_name), cv_image)   
     
+    def dataCallback(self, odom_msg, img_msg, depth_msg):
+        # self.get_logger().info(f"Depth timestamp: {depth_msg.header.stamp}")
+        self.odom_ = odom_msg
+        
+        self.rgb_image_ = img_msg
+
+
+        self.depth_image_ = depth_msg
+
+
+
     def create_arrow_marker(self, id, tail, vector):
         msg = Marker()
         msg.action = Marker.ADD
@@ -275,7 +315,7 @@ class OffboardControl(Node):
             point=np.array([0.0, 0.0, 1.0])
             dist = np.sqrt((point[0] - self.odom_.pose.pose.position.x)**2 + (point[1] - self.odom_.pose.pose.position.y)**2 + (point[2] - self.odom_.pose.pose.position.z)**2)
             if (dist > 0.5):
-                self.get_logger().info(f'Going to the (0,0,1) position.')
+                self.get_logger().info(f'Going to the (0,0,1) HOME position.')
 
             setpoint_msg = PositionTarget()
             setpoint_msg.header.stamp = self.get_clock().now().to_msg()
@@ -342,7 +382,7 @@ class OffboardControl(Node):
 
             self.setopint_pub_.publish(setpoint_msg)
 
-            if dist < 0.5:
+            if dist < 1.0:
                 self.first_point_t_ = Clock().now().nanoseconds/1000/1000/1000
                 self.reached_first_point_ = True
             self.traj_start_t_ = time()
@@ -406,7 +446,13 @@ class OffboardControl(Node):
 
         self.rgb_image_name = f"{self.file_name_}_{self.traj_counter_}_{self.offboard_setpoint_counter_}_rgb.png"
         self.depth_image_name = f"{self.file_name_}_{self.traj_counter_}_{self.offboard_setpoint_counter_}_depth.png"
-
+        
+        cv_image = CvBridge().imgmsg_to_cv2(self.rgb_image_, "bgr8")  
+        self.save_image(cv_image, self.rgb_img_directory_, "rgb")
+        
+        cv_image = CvBridge().imgmsg_to_cv2(self.depth_image_, "passthrough")  
+        self.save_image(cv_image, self.depth_img_directory_, "depth")
+       
         # TODO Save actual poision in CSV file
         self.csv_writer_.writerow([t, tx, ty, tz, self.rgb_image_name, self.depth_image_name])
         self.offboard_setpoint_counter_ += 1
@@ -425,4 +471,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
